5877690e484db6bc135c23808436a2ae1300a0a1
Saerom <saerombang11@gmail.com>
2019-11-24 20:54:38 +0900

    added TaskText style

--- src/components/Todo/style.js
+++ src/components/Todo/style.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 export const TaskText = styled.span`
+  font-weight: 500;
+  font-size: 16px;
   display: flex;
-  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  font-style: normal;
+
+  @media only screen and (min-width: 960px){
+    font-weight: 500;
+    font-size: 20px;
+    font-style: normal;
+
+  }
 `
--- src/components/buttons/StopButton/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const StopButton = props => {
- return(
-   <button onClick={props.stopCountDown}>Stop</button>
- )
-}
